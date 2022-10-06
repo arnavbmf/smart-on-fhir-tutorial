80e1635cc07f0785045cd7914f72f8519b2d7942
arnav <arnav.mf@gmail.com>
2022-10-06 20:24:39 +0530

    test

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -13,6 +13,7 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
+        console.log(pt);
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
